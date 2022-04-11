051666b484f52ff46f057309fb6184b4d80162ec
staffsterr2000 <rosenkostas@gmail.com>
2022-04-11 10:49:49 +0300

    spring initialize

---
+++ src/main/java/com/staffsterr2000/stringi/StringiApplication.java
@@ -0,0 +1,13 @@
+package com.staffsterr2000.stringi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StringiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StringiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/staffsterr2000/stringi/StringiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.staffsterr2000.stringi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StringiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
